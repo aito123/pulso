a4520c1895e420b79cfa6df86c9c7a05522c1157
aito123 <sotelog001@gmail.com>
2022-03-05 19:59:18 -0500

    what

---
+++ R/data_prueba.R
@@ -0,0 +1,7 @@
+#' Data de prueba anonimizada basada en anteriores estudios.
+#'
+#' Un dataset de 41 casos con preguntas comunes para aplicar las funciones del paquete pulso
+#' diamonds.
+#'
+#' @format Un dataset con 41 casos y 56 variables
+#' @source \url{https://pulso.pucp.edu.pe/}
